d1bed413761b5d259ff48f45b14497813bfb61a3
yjkyj3 <yjkyj3@naver.com>
2023-12-25 23:23:13 +0900

    Day1

---
+++ 28235.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string arr;
+    cin >> arr;
+    if(arr=="SONGDO"){
+        cout << "HIGHSCHOOL";
+    }
+    else if(arr=="CODE"){
+            cout << "MASTER";
+    }
+    else if(arr=="2023"){
+        cout << "0611";
+    }
+    else if(arr=="ALGORITHM"){
+        cout << "CONTEST";
+    }
+    return 0;
+}

ef37ed2ef1f3b0f5ecbe11fa85a6202748bf31d4
yjkyj3 <yjkyj3@naver.com>
2024-09-20 17:18:07 +0900

    8.26

---
+++ 11328.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+    int a;
+    cin >> a;
+    for(int i=0;i<a;i++){
+
+    }
+}
---
+++ 32326.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int r,g,b;
+    cin >> r >> g>> b;
+    cout << ((r*3)+(g*4)+(b*5));
+}
+// Created by 김영준 on 24. 9. 20.
+//

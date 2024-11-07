c35bdc3afacf776777c22335840deea34ecb1a38
yjkyj3 <yjkyj3@naver.com>
2024-11-07 15:34:26 +0900

    11.7

---
+++ 32154.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    cin >> i;
+    switch(i){
+        case 1:{
+            cout << 11<<endl;
+            cout << "A B C D E F G H J L M";
+            break;
+        }
+        case 2:{
+            cout << 9<<endl;
+            cout << "A C E F G H I L M";
+            break;
+        }
+        case 3:{
+            cout << 9<<endl;
+            cout << "A C E F G H I L M";
+            break;
+        }
+        case 4:{
+            cout << 9<<endl;
+            cout << "A B C E F G H L M";
+            break;
+        }
+        case 5:{
+            cout << 8<<endl;
+            cout << "A C E F G H L M";
+            break;
+        }
+        case 6:{
+            cout << 8<<endl;
+            cout << "A C E F G H L M";
+            break;
+        }
+        case 7:{
+            cout << 8<<endl;
+            cout << "A C E F G H L M";
+            break;
+        }
+        case 8:{
+            cout << 8<<endl;
+            cout << "A C E F G H L M";
+            break;
+        }case 9:{
+            cout << 8<<endl;
+            cout << "A C E F G H L M";
+            break;
+        }case 10:{
+            cout << 8<<endl;
+            cout << "A B C F G H L M";
+            break;
+        }
+    }
+    return 0;
+}

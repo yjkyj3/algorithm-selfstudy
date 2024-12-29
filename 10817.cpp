2aa10f5e265a58e863c4f73dd97e0626395388d2
yjkyj3 <yjkyj3@naver.com>
2024-12-29 18:02:21 +0900

    12.29

--- 10817.cpp
+++ 10817.cpp
@@ -1,3 +1,11 @@
 //
 // Created by 김영준 on 24. 12. 29.
 //
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[3];
+    cin>>arr[0]>>arr[1]>>arr[2];
+    sort(arr,arr+3);
+    cout << arr[1];
+}
